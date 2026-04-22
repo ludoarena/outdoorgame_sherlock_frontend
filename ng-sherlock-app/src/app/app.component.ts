9f3a23b0f4ff8b6a89a9cb365c2b50ceba8bdb60
ludoarena <ludovic.tambour@gmail.com>
2026-04-22 17:44:57 +0200

    add class User and mock for list of user

--- ng-sherlock-app/src/app/app.component.ts
+++ ng-sherlock-app/src/app/app.component.ts
@@ -1,26 +1,27 @@
 import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { USERS } from './mock-users';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <h1>Hello, {{ title() }}</h1>
+    <h1>List of users</h1>
 
     <router-outlet />
   `,
 })
 export class AppComponent implements OnInit {
-  users = ['toto', 'titi', 'tata'];
-  protected readonly title = signal(this.users[0]);
+  users : User[] = USERS;
 
   ngOnInit(): void {
       console.table(this.users);
       this.selectUser(this.users[0])
   }
 
-  selectUser(userName : string) {
-    console.log(`You have clicked on ${userName}`)
+  selectUser(user : User) {
+    console.log(`You have clicked on ${user.firstName} ${user.lastName}`)
   }
 }
---
+++ ng-sherlock-app/src/app/user.ts
@@ -0,0 +1,8 @@
+export class User {
+   id! : number;
+   lastName! : string;
+   firstName! : string;
+   email! : string;
+   password! : string;
+   isAdmin! : boolean;
+}
