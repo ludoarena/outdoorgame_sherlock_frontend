7bc6cb7b3f0926fd6f134cba48983dab3e438472
ludoarena <ludovic.tambour@gmail.com>
2026-04-29 14:03:23 +0200

    Add Anchor in the "not-found-page" to go to the main page

--- ng-sherlock-app/src/app/page-not-found/page-not-found.component.ts
+++ ng-sherlock-app/src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
-  template: ` <p>404 - Page not found</p> `,
+  imports: [RouterLink],
+  template: ` 
+    <div>
+      <h1>404 - Page not found</h1>
+      <a routerLink="/users">Retourner à l'accueil</a> 
+    </div>`,
   styles: ``,
 })
 export class PageNotFoundComponent {}
