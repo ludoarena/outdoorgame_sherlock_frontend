503b6660c10b45e01aeaa1bd153cbb246d64fd37
ludoarena <ludovic.tambour@gmail.com>
2026-04-29 10:25:12 +0200

    fill user detail page

--- ng-sherlock-app/src/app/detail-user/detail-user.component.ts
+++ ng-sherlock-app/src/app/detail-user/detail-user.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { USERS } from '../model/mock-users';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-detail-user',
   standalone: true,
   imports: [],
   templateUrl: './detail-user.component.html',
-  styles: ``,
+  styles: [],
 })
-export class DetailUserComponent {
+export class DetailUserComponent implements OnInit {
   @Input() id!: string;
+
+  users : User[] | undefined;
+  user : User | undefined;
+
+  ngOnInit(): void {
+      this.users = USERS;
+      if (this.id)
+      {
+        this.user = this.users.find(user => user.id === +this.id);
+      }
+  }
 }
